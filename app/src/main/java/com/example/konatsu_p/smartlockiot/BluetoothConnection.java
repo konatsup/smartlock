a3e345f06df847b70d44485f62b240e8d1775c9a
konatsup <konatsukomatsu@gmail.com>
2018-12-02 05:55:01 +0900

    Add receive feature from arduino

--- app/src/main/java/com/example/konatsu_p/smartlockiot/BluetoothConnection.java
+++ app/src/main/java/com/example/konatsu_p/smartlockiot/BluetoothConnection.java
@@ -5,33 +5,66 @@ import android.bluetooth.BluetoothSocket;
 import android.util.Log;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
+import static android.support.constraint.Constraints.TAG;
+
 public class BluetoothConnection extends Thread {
-    private final BluetoothSocket mSocket;
+    private BluetoothSocket mSocket;
     private final BluetoothDevice mDevice;
-    private final OutputStream mOutput;
-    Boolean isCheck = true;
+    private InputStream mInputStream;
+    private OutputStream mOutputStream;
+    private boolean mIsRunning;
 
     private enum State {CONNECT, CONNECTED, DISCONNECT}
 
-
     private State mState;
+    private String text = "";
 
     public BluetoothConnection(BluetoothDevice device) {
         mDevice = device;
-        BluetoothSocket socket = null;
-        OutputStream out = null;
-        try {
-            socket = mDevice.createRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"));
-            out = socket.getOutputStream();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        mSocket = socket;
-        mOutput = out;
+        mIsRunning = true;
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    mSocket = mDevice.createRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"));
+                    mSocket.connect();
+                    mInputStream = mSocket.getInputStream();
+                    mOutputStream = mSocket.getOutputStream();
+
+                    byte[] buffer = new byte[1024];
+                    int bytesRead;
+
+                    while (mIsRunning) {
+                        bytesRead = mInputStream.read(buffer);
+                        final String readMsg = new String(buffer, 0, bytesRead);
+                        if (readMsg.trim() != null && !readMsg.trim().equals("")) {
+                            Log.i(TAG, "value=" + readMsg.trim());
+
+                            String crlf = System.getProperty("line.separator");
+                            text = text + readMsg.trim() + crlf;
+
+                        } else {
+                            Log.i(TAG, "value=nodata");
+                        }
+
+                    }
+                } catch (Exception e) {
+                    Log.e(TAG, "error:" + e);
+                    try {
+                        mSocket.close();
+                    } catch (Exception ee) {
+                    }
+                    mIsRunning = false;
+                }
+            }
+        }).start();
+
         mState = State.CONNECT;
     }
 
@@ -39,7 +72,7 @@ public class BluetoothConnection extends Thread {
     public void run() {
         Log.d("LOG", "Client start");
         if (mSocket == null) return;
-        while (isCheck) {
+        while (mIsRunning) {
             switch (mState) {
                 case CONNECT:
                     try {
@@ -51,15 +84,16 @@ public class BluetoothConnection extends Thread {
                     }
                     mState = State.CONNECTED;
                     Log.d("LOG", "Connected!");
-                    isCheck = false;
+                    mIsRunning = false;
                     break;
                 case CONNECTED:
-                    isCheck = false;
+                    mIsRunning = false;
                     break;
                 case DISCONNECT:
                     if (mSocket != null) {
                         try {
-                            mOutput.write('E');
+                            mOutputStream.write('E');
+                            disconnectDevice();
                             TimeUnit.MILLISECONDS.sleep(100);
                             mSocket.close();
                             Log.d("LOG", "Disconnected");
@@ -67,7 +101,7 @@ public class BluetoothConnection extends Thread {
                             e.printStackTrace();
                         }
                     }
-                    isCheck = false;
+                    mIsRunning = false;
                     break;
 
             }
@@ -77,7 +111,33 @@ public class BluetoothConnection extends Thread {
     public void send(byte sendData[]) {
         if (!mState.equals(State.CONNECTED)) return;
         try {
-            mOutput.write(sendData);
+            mOutputStream.write(sendData);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void disconnectDevice() {
+
+        if (mDevice == null) {
+            return;
+        }
+
+        try {
+            if (mSocket != null) {
+                mSocket.close();
+                mSocket = null;
+            }
+
+            if (mInputStream != null) {
+                mInputStream.close();
+                mInputStream = null;
+            }
+
+            if (mOutputStream != null) {
+                mOutputStream.close();
+                mOutputStream = null;
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
