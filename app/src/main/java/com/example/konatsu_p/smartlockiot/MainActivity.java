b75e40f6c515b03bb52b83ba2361019603f80f64
konatsup <konatsukomatsu@gmail.com>
2018-12-01 19:31:41 +0900

    Add Firebase Database and getDataValue

--- app/src/main/java/com/example/konatsu_p/smartlockiot/MainActivity.java
+++ app/src/main/java/com/example/konatsu_p/smartlockiot/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.konatsu_p.smartlockiot;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 public class MainActivity extends AppCompatActivity {
+    Boolean isLock = true;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textview);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference userRef = database.getReference("DEVICE_ID").child("Status");
+        userRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(final DataSnapshot dataSnapshot) {
+                isLock = dataSnapshot.getValue(Boolean.class);
+                textView.setText(isLock.toString());
+            }
+
+            @Override
+            public void onCancelled(final DatabaseError databaseError) {
+            }
+        });
+
     }
 }
